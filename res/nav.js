6fc557891047655da1d60cc5098960268180f336
Edmerson Pizarra <edmerpizarra@gmail.com>
2021-08-31 18:40:31 +0800

    format changes

--- res/nav.js
+++ res/nav.js
@@ -3,19 +3,19 @@ const admissBtn = document.getElementById("admission-btn");
 const coursesBtn = document.getElementById("course-btn");
 const contactBtn = document.getElementById("contact-btn");
 const learnMoreBtn = document.getElementById("le-more");
-homeBtn.onclick = function (){
-    location.href="/"
-    console.log("Navigated to Home")
+homeBtn.onclick = function () {
+  location.href = "/";
+  console.log("Navigated to Home");
 };
-admissBtn.onclick = function (){
-    location.href="/admission.html"
-}
-coursesBtn.onclick = function (){
-    location.href="/courses.html"
-}
-contactBtn.onclick = function (){
-    location.href="/contact.html"
-}
-learnMoreBtn.onclick = function (){
-    location.href="/admission.html"
-}
+admissBtn.onclick = function () {
+  location.href = "/admission.html";
+};
+coursesBtn.onclick = function () {
+  location.href = "/courses.html";
+};
+contactBtn.onclick = function () {
+  location.href = "/contact.html";
+};
+learnMoreBtn.onclick = function () {
+  location.href = "/admission.html";
+};
